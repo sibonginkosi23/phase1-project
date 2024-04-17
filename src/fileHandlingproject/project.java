6736af33419638cdca6c14b5334c913e02fb8995
sibonginkosi23 <mashelesbonginkosi@gmail.com>
2024-04-17 13:20:23 +0200

    i fixed the deletion and added a listing part for ascending and descedning

--- src/fileHandlingproject/project.java
+++ src/fileHandlingproject/project.java
@@ -3,6 +3,8 @@ package fileHandlingproject;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class project {
@@ -34,8 +36,9 @@ public class project {
 			ch=scanner1.nextInt();
 			switch(ch)
 			{
+			
+			//case1 file creation
 			case 1:
-				
 				System.out.println("Enter the file name to create :");
 				
 				fname=scanner1.next();
@@ -59,8 +62,9 @@ public class project {
 					}
 				}
 				break;
-			case  2:
 				
+			//case 2 write in file
+			case  2:
 				System.out.println("Enter the file name where you wanna write ");
 				fname=scanner1.next();
 				
@@ -73,11 +77,11 @@ public class project {
 				fw.write(content);
 				
 				System.out.println("Data Written in the file successfully ");
-				
 				fw.close();
 				break;
-			case 3:
 				
+			//case 3 read files
+			case 3:
 				System.out.println("Enter the file name whose data you wanna read ");
 				fname=scanner1.next();
 				
@@ -91,10 +95,9 @@ public class project {
 				}
 				
 				readSc.close();
-				
-				
-				
 				break;
+				
+			//case 4 deletion
 			case 4:
 				
 				System.out.println("Enter file name which you wanna delete ");
@@ -102,20 +105,25 @@ public class project {
 				
 				File f3 = new File("C:\\Users\\Mashe\\eclipse-workspace\\fileHandlingproject\\src\\fileHandlingproject\\filesoperation\\"+fname+".txt");
 				
-				f3.delete();
 				
-				if(f3.exists())
-				{
-					System.out.println("Error found in deleting the file ");
-				}
-				else
-				{
-					System.out.println("File deleted uccessfully ");
-				}
-				
+				if (!f3.exists()) {
+			        System.out.println("**File name is either incorrect or file does not exist! ");
+			        break;
+			    }
+
+			    if (f3.delete()) {
+			        System.out.println("File deleted successfully!");
+			    } else {
+			        System.out.println("Error found in deleting the file!");
+			    }
+
+			    if (!f3.exists()) {
+			        System.out.println("File does not exist after deletion.");
+			    }
 				break;
-			case 5:
 				
+			//case 5 searching
+			case 5:
 				System.out.println("Enter the file name for searching ");
 				fname=scanner1.next();
 				
@@ -145,40 +153,82 @@ public class project {
 						System.out.println("file not found ");
 					}
 				}
-				
-				
-				
 				break;
+				
+			//case 6
 			case 6:
-				
-				System.out.println("List of Files and Folders are :");
-				
-				File f6 = new File("C:\\Users\\Mashe\\eclipse-workspace\\fileHandlingproject\\src\\fileHandlingproject\\filesoperation\\");
-				
-				File[] l1 = f6.listFiles();
-				
-				for(File fi: l1)
-				{
-					if(fi.isDirectory())
-					{
-						System.out.println(fi.getName());
-					}
-					else if(fi.isFile())
-					{
-						System.out.println(fi.getName());
-					}
-				}
-				break;
+				Scanner scanner = new Scanner(System.in);
+		        String rootDirectory = "C:\\Users\\Mashe\\eclipse-workspace\\fileHandlingproject\\src\\fileHandlingproject\\filesoperation\\";
+		        
+		        int choice;
+		        do {
+		            System.out.println("1. List files and folders");
+		            System.out.println("0. Exit");
+		            System.out.print("Enter your choice: ");
+		            choice = scanner.nextInt();
+		            
+		            switch (choice) {
+		                case 1:
+		                    System.out.println("Enter sorting order (asc/desc), or enter 0 to return to the menu: ");
+		                    String sortOrder = scanner.next();
+		                    if (sortOrder.equals("0")) {
+		                        break;
+		                    }
+		                    listFilesAndFolders(rootDirectory, sortOrder);
+		                    break;
+		                case 0:
+		                    System.out.println("Exiting...");
+		                    break;
+		                default:
+		                    System.out.println("Invalid choice.");
+		                    break;
+		            }
+		        } while (choice != 0);
+		        
+		    //case 7    
 			case 7:
 				System.out.println("Thank you for using the Application  ");
 				System.exit(ch);
 				break;
 			default:
-				System.out.println("zPlease enter a valid option ");
+				System.out.println("Please enter a valid option ");
 			}
 		}
 		while(true);
 
 	}
+	private static void listFilesAndFolders(String rootDirectory, String sortOrder) {
+        File directory = new File(rootDirectory);
+
+        if (!directory.exists() || !directory.isDirectory()) {
+            System.out.println("Invalid directory path.");
+            return;
+        }
+
+        File[] files = directory.listFiles();
+
+        if (files == null || files.length == 0) {
+            System.out.println("No files or folders found in the directory.");
+            return;
+        }
+
+        if (sortOrder.equalsIgnoreCase("asc")) {
+            Arrays.sort(files);
+        } else if (sortOrder.equalsIgnoreCase("desc")) {
+            Arrays.sort(files, Collections.reverseOrder());
+        } else {
+            System.out.println("Invalid sorting order.");
+            return;
+        }
+
+        System.out.println("List of Files and Folders:");
+
+        for (File file : files) {
+            if (file.isDirectory() || file.isFile()) {
+                System.out.println(file.getName());
+            }
+        }
+    }
+
 
 }
